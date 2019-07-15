c47b64a3d4c9090d3b59fcf59764150117038563
Kevin Pierce <kevinkayaks@gmail.com>
2019-07-15 01:23:23 -0700

    finally got lisle pinned down

---
+++ tile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# this script makes the two figures from their components
+montage sim-vs-theory-lisle-mean.png sim-vs-theory-lisle.png -mode Concatenate -tile 2x1 combo1.png
+montage Lisle1998-behavior.png lisle-initial-conditions.png -mode Concatenate -tile 2x1 combo2.png
